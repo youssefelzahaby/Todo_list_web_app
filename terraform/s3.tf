e90536754d0eb5985373c2246af34ef892a9ff09
youssefelzahaby <yousefdahaby110@gmail.com>
2026-09-12 20:48:48 +0300

    terrafrom  mod

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,19 +1,36 @@
 resource "aws_s3_bucket" "frontend" {
+  force_destroy = true
   bucket = "todo-frontend-pro"
 
   tags = {
     Name        = "todo-frontend-pro"
     Environment = "pro"
   }
+  
 }
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "login.html"
+  }
+
+  error_document {
+    key = "login.html"
+  }
+}
+
+
 
 resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_versioning" "frontend" {
@@ -23,3 +40,28 @@ resource "aws_s3_bucket_versioning" "frontend" {
     status = "Enabled"
   }
 }
+resource "aws_s3_bucket_policy" "frontend" {
+
+  bucket = aws_s3_bucket.frontend.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.frontend
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = "*"
+
+        Action = "s3:GetObject"
+
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+}
+
