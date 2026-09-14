9ebf2d4313fe1b34fa80ee74e14dd1f79ab97c72
youssefelzahaby <yousefdahaby110@gmail.com>
2026-09-14 17:07:58 +0300

    terraform ASG mod

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,6 @@ output "alb_dns_name" {
 output "ecr_repository_url" {
   value = aws_ecr_repository.todo_backend.repository_url
 }
+output "secret_arn" {
+  value = aws_secretsmanager_secret.backend_credentials.arn
+}
